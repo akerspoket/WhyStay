ff9f84dfea3d37b2053e55705f9a5875ee46cea0
Akerspoket <konrad_o_92@hotmail.com>
2016-08-05 23:00:07 +0200

    Structuring and picking of grid
    We can currently place stuff in the world

---
+++ Assets/Scripts/GridHandler.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridHandler : MonoBehaviour {
+
+    // Public variables
+    [Header("Grid size option")]
+    [Tooltip("Used for deciding the size of the grid")]
+    public GameObject groundPlane;
+    [Tooltip("Number of cells in Z")]
+    public float numberOfCellsZ;
+    [Tooltip("Number of cells in X")]
+    public float numberOfCellsX;
+    
+    public int currentlySelectedGridX { get; set; }
+    public int currentlySelectedGridZ { get; set; }
+    // Private variables
+    float gridSizeZ;
+    float gridSizeX;
+
+    float cellSizeZ;
+    float cellSizeX;
+    // Unity functions
+
+	// Use this for initialization
+	void Start () {
+        Vector3 t_groundScale = groundPlane.transform.lossyScale;
+        t_groundScale *= 10;
+        gridSizeX = t_groundScale.x;
+        gridSizeZ = t_groundScale.z;
+
+        // Calculate how big each cell can be
+        cellSizeZ = gridSizeZ / numberOfCellsZ;
+        cellSizeX = gridSizeX / numberOfCellsX;
+
+        //LightUpCell(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+    // Private functions;
+    public void TransformPositionToCellNumber(Vector3 p_position, out int o_cellNumberX, out int o_cellNumberZ)
+    {
+        float t_positionX = p_position.x;
+        float t_positionZ = p_position.z;
+
+        o_cellNumberX = Mathf.FloorToInt((t_positionX + gridSizeX * 0.5f - cellSizeX * 0.5f) / cellSizeX + 0.5f);
+        o_cellNumberZ = Mathf.FloorToInt((t_positionZ + gridSizeZ * 0.5f - cellSizeZ * 0.5f) / cellSizeZ + 0.5f);
+    }
+
+    // out variables will contain the origo of the cell
+    public void TransformCellNumberToPosition(int p_cellNumberX, int p_cellNumberZ, out Vector3 o_position)
+    {
+        float t_positionX = (float)p_cellNumberX * cellSizeX - gridSizeX * 0.5f + cellSizeX * 0.5f;
+        float t_positionZ = (float)p_cellNumberZ * cellSizeZ - gridSizeZ * 0.5f + cellSizeZ * 0.5f;
+        o_position = new Vector3(t_positionX, 0, t_positionZ); 
+    }
+}
---
+++ Assets/Scripts/MapPicking.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapPicking : MonoBehaviour {
+    public GameObject gameField; // The plane that covers the whole world 
+    public GameObject buildingToPlace;
+
+    [Header("Cell visualization")]
+    [Tooltip("Offset of how far up the cell highlighter will be placed")]
+    public float highlightOffsetY;
+    [Tooltip("Cell highlighter")]
+    public GameObject cellHighlighter;
+
+    // Private
+    GridHandler gridHandler;
+    // Use this for initialization
+    void Start () {
+        gridHandler = transform.GetComponent<GridHandler>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 t_hitPosition;
+        Ray t_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float t_distance = 0;
+        Plane gamePlane = new Plane(new Vector3(0, 1, 0), -gameField.transform.position.y);
+        if (gamePlane.Raycast(t_ray, out t_distance))
+        {
+            t_hitPosition = t_ray.GetPoint(t_distance);
+            LightUpCell(t_hitPosition);
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 t_positionToPlaceBuilding;
+                gridHandler.TransformCellNumberToPosition(gridHandler.currentlySelectedGridX, gridHandler.currentlySelectedGridZ, out t_positionToPlaceBuilding);
+                Instantiate(buildingToPlace, t_positionToPlaceBuilding, Quaternion.identity);
+            }     
+        }
+
+        
+	}
+
+    // Will not use Y dimension,  will also set currently active cell TODO maybee a bit wierd that it set the active
+    private void LightUpCell(Vector3 p_position)
+    {
+        int t_cellNumberX;
+        int t_cellNumberZ;
+        Vector3 t_positionToLightUp;
+        gridHandler.TransformPositionToCellNumber(p_position, out t_cellNumberX, out t_cellNumberZ);
+        gridHandler.TransformCellNumberToPosition(t_cellNumberX, t_cellNumberZ, out t_positionToLightUp);
+        gridHandler.currentlySelectedGridX = t_cellNumberX;
+        gridHandler.currentlySelectedGridZ = t_cellNumberZ;
+        t_positionToLightUp.y = highlightOffsetY;
+        cellHighlighter.transform.position = t_positionToLightUp;
+    }
+}
